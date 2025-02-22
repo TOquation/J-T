83a9a44ad728987a895e44b658bbfac6b3088b7a
Jason2-d <nelsonchima042@gmail.com>
2025-02-22 12:07:13 +0100

    added something

---
+++ src/app/(guest)/guest/mybookings/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, inventore.</h1>
+    </div>
+  )
+}
+
+export default page
