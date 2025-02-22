7563868efcf6076e199c09d877609998e265b7a2
TOquation <oparajitee4@gmail.com>
2025-02-22 12:25:47 +0100

    na me do am

--- src/app/(guest)/guest/mybookings/page.tsx
+++ src/app/(guest)/guest/mybookings/page.tsx
@@ -5,6 +5,7 @@ const page = () => {
     <div>
       <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, inventore.</h1>
       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis quibusdam quae corrupti aut! Laboriosam dolorem assumenda eum! Sit omnis perferendis unde enim asperiores itaque repellendus, provident quod eius. Nemo explicabo earum in expedita nisi, minima soluta molestiae iste sapiente eius animi eos fuga repellat hic vel odio deserunt corrupti et assumenda. Tempore aliquam sunt vero inventore excepturi cum dolorum adipisci magni. Totam voluptate fuga a nulla error debitis asperiores minus et, sapiente nam dolorum? Ab, voluptates sequi? Vel eaque, consequatur sunt quos, a odit aliquam eius reiciendis molestiae nisi aperiam vitae ea praesentium eos est! Incidunt suscipit doloribus debitis laudantium.
+      <p>i am chibutata</p>
     </div>
   )
 }
