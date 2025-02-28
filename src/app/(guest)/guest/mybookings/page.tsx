ac9fe4f13d54e0a8ee3b8f84c04fbe68ccfef3c0
Jason2-d <nelsonchima042@gmail.com>
2025-02-28 18:14:44 +0100

    Updated my bookings

--- src/app/(guest)/guest/mybookings/page.tsx
+++ src/app/(guest)/guest/mybookings/page.tsx
@@ -1,13 +1,82 @@
-import React from 'react'
+'use client';
+
+import React, { useState } from 'react';
+// import { ChevronRight } from 'react-icons/fi';
+// import DatePicker from '@/components/dates/date';
+// import Tab from '@/components/content_tab/tab';
+import BookingTable from '@/components/bookingtable/table';
 
 const page = () => {
-  return (
-    <div>
-      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, inventore.</h1>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis quibusdam quae corrupti aut! Laboriosam dolorem assumenda eum! Sit omnis perferendis unde enim asperiores itaque repellendus, provident quod eius. Nemo explicabo earum in expedita nisi, minima soluta molestiae iste sapiente eius animi eos fuga repellat hic vel odio deserunt corrupti et assumenda. Tempore aliquam sunt vero inventore excepturi cum dolorum adipisci magni. Totam voluptate fuga a nulla error debitis asperiores minus et, sapiente nam dolorum? Ab, voluptates sequi? Vel eaque, consequatur sunt quos, a odit aliquam eius reiciendis molestiae nisi aperiam vitae ea praesentium eos est! Incidunt suscipit doloribus debitis laudantium.
-      <p>i am chibutata</p>
-    </div>
-  )
-}
+  const [tab, setTab] = useState('active');
+  
 
-export default page
+  return (
+    <div className="container mx-auto">
+      {/* Tab container */}
+      <div className="m-5">
+        {/* Tabs navigation */}
+        <h1>My Bookings</h1>
+        <div className="flex w-full justify-between space-x-4 rounded-md bg-slate-200">
+          <div>
+            <button
+              className={`rounded-md px-8 py-2 text-left ${tab === 'active' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'}`}
+              onClick={() => setTab('active')}
+            >
+              Active
+            </button>
+            <button
+              className={`rounded-md px-8 py-2 text-left ${tab === 'pending' ? 'bg-yellow-500 text-white' : 'bg-gray-200 text-gray-700'}`}
+              onClick={() => setTab('pending')}
+            >
+              Pending
+            </button>
+            <button
+              className={`rounded-md px-8 py-2 text-left ${tab === 'concluded' ? 'bg-green-500 text-white' : 'bg-gray-200 text-gray-700'}`}
+              onClick={() => setTab('concluded')}
+            >
+              Concluded
+            </button>
+          </div>
+          <div className="">
+            <button className="rounded-md bg-emerald-500 px-8 py-2 text-left text-white">
+              Create New
+            </button>
+          </div>
+        </div>
+
+        {/* Vertical date picker */}
+        {/* <div className="w-full justify-between bg-white p-4 border rounded-md shadow-md">
+      {/* <input type="text" className="date-picker w-full" placeholder="Select Date" /> */}
+        {/* </div> */}
+        {/* <DatePicker /> */}
+
+        {/* Tab content */}
+        <div className="flex-1 rounded-md bg-white p-6 shadow-md">
+          {tab === 'active' && (
+            <div className="tab-content">
+              <h2 className="text-2xl font-semibold">Active Tab Content</h2>
+              <p>Here is the content for the Active tab.</p>
+              <BookingTable />
+            </div>
+          )}
+          {tab === 'pending' && (
+            <div className="tab-content">
+              <h2 className="text-2xl font-semibold">Pending Tab Content</h2>
+              <p>Here is the content for the Pending tab.</p>
+            </div>
+          )}
+          {tab === 'concluded' && (
+            <div className="tab-content">
+              <h2 className="text-2xl font-semibold">Concluded Tab Content</h2>
+              <p>Here is the content for the Concluded tab.</p>
+
+              {/* <Tab /> */}
+            </div>
+          )}
+        </div>
+      </div>
+      </div>
+  );
+};
+
+export default page;
